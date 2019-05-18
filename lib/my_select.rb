df4041e940fc0e28af64ebd4503385c8cf189c68
“Patrick <pctennislessons@gmail.com>
2019-05-18 10:56:09 -0600

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,7 @@ def my_select(collection)
     i = 0
     selection = []
     while i < collection.length
-        yield(collection[i])
-        if collection[i].even?
+        if yield(collection[i])
             selection << collection[i]
         end
         i = i + 1
