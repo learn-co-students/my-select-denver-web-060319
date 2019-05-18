5bfbb54669e6302deb5e52e0d812d9857da85fea
“Patrick <pctennislessons@gmail.com>
2019-05-18 10:41:33 -0600

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
- # your code here!
+    i = 0
+    selection = []
+    while i < collection.length
+        yield(collection[i])
+        if collection[i].even?
+            selection << collection[i]
+        end
+        i = i + 1
+    end
+    selection
 end
+
+#if it passes a test that is true it adds it to an selection array.
+#if test
+# it returns an array with out changing the oringial. 
+# it runs 
