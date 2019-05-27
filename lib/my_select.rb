12108b1238a708213a44998d46daf4f436dcadef
foggybrains <dustin.keyes@gmail.com>
2019-05-27 17:55:51 -0600

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+    if block_given?  
+        i = 0
+        true_objs = []
+        while i < collection.length
+            new_var = yield(collection[i])
+                if new_var == true
+                    true_objs << collection[i]
+                end
+            i = i + 1
+        end
+        true_objs
+    else
+        nil
+    end
 end
